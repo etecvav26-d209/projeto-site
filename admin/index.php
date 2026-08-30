359247198f87e8609f0c88859e7671057b220634
Laura Duarte <dduartelaureta@gmail.com>
2026-08-30 13:13:25 -0300

    Adicionando section

--- admin/index.php
+++ admin/index.php
@@ -3,3 +3,14 @@
 require_once "../includes/header.php";
 
 ?>
+
+<section class="admin-painel">
+
+    <h1>Painel Administrativo</h1>
+
+    <p>
+        Selecione uma opção.
+    </p>
+
+
+</section>
