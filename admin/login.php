6987031f0bdbc355f5ddcf8ebb771fce70616b04
Laura Duarte <dduartelaureta@gmail.com>
2026-08-30 14:27:07 -0300

    criando página de login

---
+++ admin/login.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "../includes/header.php";
+
+?>
+
+<section class="admin-painel">
+
+    <h1>Login Administrativo</h1>
+
+    <p>
+        Área de acesso administrativo.
+    </p>
+
+</section>
+
+<?php
+
+require_once "../includes/footer.php";
+
+?>
