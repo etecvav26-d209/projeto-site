b870c5e63e9e4bf72c00d255bd78cc277439196f
Laura Duarte <dduartelaureta@gmail.com>
2026-08-30 18:44:57 -0300

    adicionando valores

--- admin/usuarios/cadastrar.php
+++ admin/usuarios/cadastrar.php
@@ -5,5 +5,10 @@ require_once "../../includes/header.php";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $tipo = $_POST['tipo'];
 }
 ?>
