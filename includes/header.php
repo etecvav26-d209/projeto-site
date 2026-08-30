02f50ec390d4f8c10974981098ccb9b5f8c1ad7a
Pedro Coraine <corainepedro3@gmail.com>
2026-08-29 23:48:51 -0300

    adicionando navbar

--- includes/header.php
+++ includes/header.php
@@ -15,8 +15,15 @@
 <header class="header">
 
 <div class="logo">   
-        <img src="img/logo/logo.png" alt="Logo Noir & Sugar">
+        <img src="logo/logo-sem-fundo.png" alt="Logo Noir & Sugar">
 </div>
+
+ <nav class="nav">
+        <a href="index.php#inicio">Início</a>
+        <a href="index.php#galeria">Galeria</a>
+        <a href="index.php#cardapio">Cardápio</a>
+        <a href="encomendas.php">Carrinho</a>
+    </nav>
 
 </header>
 
