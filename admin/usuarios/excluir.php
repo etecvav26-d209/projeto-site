656406c06fcd38807c9deb4efbdcae72d1e5750a
Laura Duarte <dduartelaureta@gmail.com>
2026-08-31 01:16:44 -0300

    adicionando o delete e try/catch

--- admin/usuarios/excluir.php
+++ admin/usuarios/excluir.php
@@ -6,5 +6,25 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])) {
 
  $id = $_POST['id'];
 
+ $sql = "DELETE FROM usuarios WHERE id = :id";
+
+    $stmt = $conexao->prepare($sql);
+
+    try {
+
+        $stmt->execute([
+
+            ':id' => $id
+
+        ]);
+
+        echo "Usuário excluído com sucesso!";
+
+    } catch(PDOException $erro) {
+
+        echo "Não foi possível excluir o usuário.";
+
+    }
+
 }
 ?>
