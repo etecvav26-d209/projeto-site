ccce26ef8b05cf5516ba10e9072f3a3f4ce0ceb5
Pedro Henrique <pedrorodriguescoraine@gmail.com>
2026-08-31 02:25:44 -0300

    criando excluir

--- admin/produtos/excluir.php
+++ admin/produtos/excluir.php
@@ -1,1 +1,31 @@
+<?php
 
+require_once '../../config/conexao.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])) {
+
+    $id = $_POST['id'];
+
+    $sql = "DELETE FROM produtos WHERE id = :id";
+
+    $stmt = $conexao->prepare($sql);
+
+    try {
+
+        $stmt->execute([
+
+            ':id' => $id
+
+        ]);
+
+        echo "Produto excluído com sucesso!";
+
+    } catch(PDOException $erro) {
+
+        echo "Não foi possível excluir o produto.";
+
+    }
+
+}
+
+?>
