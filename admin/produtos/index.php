b3f400eb1468624f2273e38decc7488209c6fee7
Pedro henrique <pedrorodriguescoraine@gmail.com>
2026-08-31 09:19:09 -0300

    adicionando tabelinha

--- admin/produtos/index.php
+++ admin/produtos/index.php
@@ -18,3 +18,56 @@ $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <a href="cadastrar.php">
     Cadastrar Produto
 </a>
+
+<br><br>
+
+
+<?php foreach($produtos as $produto) { ?>
+
+    <div>
+
+        <h3>
+            <?php echo $produto['nome']; ?>
+        </h3>
+
+        <p>
+            <?php echo $produto['descricao']; ?>
+        </p>
+
+        <p>
+            Preço: R$
+            <?php echo $produto['preco']; ?>
+        </p>
+
+        <p>
+            Categoria:
+            <?php echo $produto['categoria']; ?>
+        </p>
+
+        <p>
+            Disponibilidade:
+
+            <?php
+
+            if($produto['disponivel'] == 1) {
+
+                echo "Disponível";
+
+            } else {
+
+                echo "Indisponível";
+
+            }
+
+            ?>
+
+        </p>
+
+        <img
+            src="../../<?php echo $produto['imagem']; ?>"
+            width="150"
+        >
+
+        <br><br>
+
+        
