4fd8e719e6eb9b5aeb32666967b8f028737ddccd
Pedro Henrique <pedrorodriguescoraine@gmail.com>
2026-08-31 02:17:19 -0300

    adicionando if e else

--- admin/produtos/editar.php
+++ admin/produtos/editar.php
@@ -21,4 +21,81 @@ $stmt->execute([
 
 ]);
 
+$produto = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['nome'])) {
+
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $preco = $_POST['preco'];
+    $categoria = $_POST['categoria'];
+    $disponivel = $_POST['disponivel'];
+
+
+    if(isset($_FILES['imagem']) && $_FILES['imagem']['error'] == 0) {
+
+        $nomeImagem = $_FILES['imagem']['name'];
+
+        $caminho = '../../img/produtos/' . $nomeImagem;
+
+        $caminhoBanco = 'img/produtos/' . $nomeImagem;
+
+        move_uploaded_file(
+            $_FILES['imagem']['tmp_name'],
+            $caminho
+        );
+
+        $imagem = $caminhoBanco;
+
+    } else {
+
+        $imagem = $produto['imagem'];
+
+    }
+
+
+    $sql = "UPDATE produtos SET
+
+        nome = :nome,
+        descricao = :descricao,
+        preco = :preco,
+        imagem = :imagem,
+        categoria = :categoria,
+        disponivel = :disponivel
+
+        WHERE id = :id";
+
+
+    $stmt = $conexao->prepare($sql);
+
+
+    $stmt->execute([
+
+        ':nome' => $nome,
+        ':descricao' => $descricao,
+        ':preco' => $preco,
+        ':imagem' => $imagem,
+        ':categoria' => $categoria,
+        ':disponivel' => $disponivel,
+        ':id' => $id
+
+    ]);
+
+
+    echo "Produto atualizado com sucesso!";
+
+
+    $sql = "SELECT * FROM produtos WHERE id = :id";
+
+    $stmt = $conexao->prepare($sql);
+
+    $stmt->execute([
+
+        ':id' => $id
+
+    ]);
+
+    $produto = $stmt->fetch(PDO::FETCH_ASSOC);
+}
 ?>
