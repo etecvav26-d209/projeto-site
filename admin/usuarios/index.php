d735ee55da2f7675a29a905d37573806258e5e16
Laura Duarte <dduartelaureta@gmail.com>
2026-08-31 01:24:59 -0300

    add config do banco e statement

--- admin/usuarios/index.php
+++ admin/usuarios/index.php
@@ -4,4 +4,14 @@ require_once '../../config/conexao.php';
 
 include '../../includes/header.php';
 
+$sql = "SELECT * FROM usuarios";
+
+$stmt = $conexao->prepare($sql);
+
+$stmt->execute();
+
+$usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
+
+
