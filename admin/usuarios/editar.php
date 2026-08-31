dc21393400ef7369c55d1ef75551e366c237307d
Laura Duarte <dduartelaureta@gmail.com>
2026-08-31 00:40:02 -0300

    adicionando campo para atualização

--- admin/usuarios/editar.php
+++ admin/usuarios/editar.php
@@ -23,6 +23,17 @@ $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['nome'])) {
 
+$nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $tipo = $_POST['tipo'];
+
+    $sql = "UPDATE usuarios SET
+
+    nome = :nome,
+    email = :email,
+    tipo = :tipo
+
+    WHERE id = :id";
 
 }
 ?>
