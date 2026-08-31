da516deabdde148c1de2e6b8bf4c4b67423496d8
Laura Duarte <dduartelaureta@gmail.com>
2026-08-30 23:07:01 -0300

    adicionando campos

--- admin/usuarios/cadastrar.php
+++ admin/usuarios/cadastrar.php
@@ -43,3 +43,72 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <form method="POST">
 
+<?php
+
+require_once '../../config/conexao.php';
+
+include '../../includes/header.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $tipo = $_POST['tipo'];
+
+    $sql = "INSERT INTO usuarios
+        (nome, email, senha, tipo)
+        VALUES
+        (:nome, :email, :senha, :tipo)";
+
+    $stmt = $conexao->prepare($sql);
+
+    $stmt->execute([
+
+        ':nome' => $nome,
+        ':email' => $email,
+        ':senha' => $senha,
+        ':tipo' => $tipo
+
+    ]);
+
+    $id = $conexao->lastInsertId();
+
+    if($id) {
+
+        echo "Usuário cadastrado com sucesso! ID: " . $id;
+
+    }
+
+}
+
+?>
+
+<h2>Cadastrar Usuário</h2>
+
+<form method="POST">
+
+    <input
+        type="text"
+        name="nome"
+        placeholder="Nome do usuário"
+        required
+    >
+
+    <br><br>
+
+    <input
+        type="email"
+        name="email"
+        placeholder="E-mail"
+        required
+    >
+
+    <br><br>
+
+    <input
+        type="password"
+        name="senha"
+        placeholder="Senha"
+        required
+    >
