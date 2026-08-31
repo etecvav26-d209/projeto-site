6bc0090b78f3adcbbab4b972509cf64c3f4983fa
Nicolas Saraiva <saraivabatistan@gmail.com>
2026-08-31 01:47:37 -0300

    adicionando condição e variaveis

--- admin/produtos/cadastrar.php
+++ admin/produtos/cadastrar.php
@@ -4,4 +4,22 @@ require_once '../../config/conexao.php';
 
 include '../../includes/header.php';
 
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $preco = $_POST['preco'];
+    $categoria = $_POST['categoria'];
+    $disponivel = $_POST['disponivel'];
+
+    $imagem = $_FILES['imagem'];
+
+    $nomeImagem = $imagem['name'];
+
+    $caminho = '../../img/produtos/' . $nomeImagem;
+
+    $caminhoBanco = 'img/produtos/' . $nomeImagem;
+
+}
 ?>
