f566c1dbc64482eabf110ccb6d105760367cb5d2
Pedro henrique <pedrorodriguescoraine@gmail.com>
2026-08-31 09:05:41 -0300

    adicionando configs do banco

--- admin/produtos/index.php
+++ admin/produtos/index.php
@@ -4,4 +4,11 @@ require_once '../../config/conexao.php';
 
 include '../../includes/header.php';
 
+$sql = "SELECT * FROM produtos";
+
+$stmt = $conexao->prepare($sql);
+
+$stmt->execute();
+
+$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
