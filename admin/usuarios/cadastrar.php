46d5973441012a6cd8f148875aeee4cb34afc76c
Laura Duarte <dduartelaureta@gmail.com>
2026-08-30 23:19:35 -0300

    removendo erro

--- admin/usuarios/cadastrar.php
+++ admin/usuarios/cadastrar.php
@@ -39,50 +39,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 ?>
 
-<h2>Cadastrar Usuário</h2>
-
-<form method="POST">
-
-<?php
-
-require_once '../../config/conexao.php';
-
-include '../../includes/header.php';
-
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-    $tipo = $_POST['tipo'];
-
-    $sql = "INSERT INTO usuarios
-        (nome, email, senha, tipo)
-        VALUES
-        (:nome, :email, :senha, :tipo)";
-
-    $stmt = $conexao->prepare($sql);
-
-    $stmt->execute([
-
-        ':nome' => $nome,
-        ':email' => $email,
-        ':senha' => $senha,
-        ':tipo' => $tipo
-
-    ]);
-
-    $id = $conexao->lastInsertId();
-
-    if($id) {
-
-        echo "Usuário cadastrado com sucesso! ID: " . $id;
-
-    }
-
-}
-
-?>
 
 <h2>Cadastrar Usuário</h2>
 
