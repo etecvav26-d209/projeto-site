0f46f4e0168c018b896db1468dbe07620611bf2f
Laura Duarte <dduartelaureta@gmail.com>
2026-09-18 16:16:04 -0300

    mudando caminho

--- admin/pedidos/visualizar.php
+++ admin/pedidos/visualizar.php
@@ -82,7 +82,7 @@ $sql = "SELECT pedidos.*, usuarios.nome, usuarios.email
                 <p>Este pedido não possui produtos.</p> 
             <?php endif; ?>
 
-<a href="index.php">Voltar</a>
+<a href="../index.php">Voltar</a>
 
 <?php
 
