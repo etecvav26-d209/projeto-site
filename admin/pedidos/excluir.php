9befa48a7037aafd7eec5cf18be7d5e2c3b01a96
Laura Duarte <dduartelaureta@gmail.com>
2026-09-18 17:51:18 -0300

    adicionando o deletar se confirmado

--- admin/pedidos/excluir.php
+++ admin/pedidos/excluir.php
@@ -7,4 +7,17 @@ if (!isset($_POST['id'])) {
 } 
 
 $id = $_POST['id'];
+
+if (isset($_POST['confirmar'])) { 
+    $sql_itens = "DELETE FROM itens_pedido 
+        WHERE pedido_id = :pedido_id"; 
+        
+    $stmt_itens = $conexao->prepare($sql_itens); 
+    $stmt_itens->execute([ ':pedido_id' => $id ]); 
+    $sql = "DELETE FROM pedidos WHERE id = :id";
+    $stmt = $conexao->prepare($sql); 
+    $stmt->execute([ ':id' => $id ]); 
+        echo "Pedido excluído com sucesso."; 
+    exit; 
+}
 ?>
