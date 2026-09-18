356a34023f7962cdffb0bc71ebb825e3ba67e7c3
Laura Duarte <dduartelaureta@gmail.com>
2026-09-18 20:59:42 -0300

    adicionando comando sql

--- admin/pedidos/index.php
+++ admin/pedidos/index.php
@@ -2,4 +2,10 @@
 require_once '../../config/conexao.php';
 include '../../includes/header.php';
 
+$sql = "SELECT pedidos.*, usuarios.nome
+        FROM pedidos
+        INNER JOIN usuarios
+        ON pedidos.usuario_id = usuarios.id
+        ORDER BY pedidos.id DESC";
+
 ?>
