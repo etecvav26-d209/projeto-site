ddbcc84249d9f3560dcefc5cd78e69b0430d2075
Laura Duarte <dduartelaureta@gmail.com>
2026-09-18 17:47:34 -0300

    adicionando verificação do id

--- admin/pedidos/excluir.php
+++ admin/pedidos/excluir.php
@@ -1,3 +1,10 @@
 <?php require_once '../../config/conexao.php';
 include '../../includes/header.php';
+
+if (!isset($_POST['id'])) { 
+    echo "ID do pedido não informado."; 
+    exit; 
+} 
+
+$id = $_POST['id'];
 ?>
