c1ed28587c167a52b31d0ba0e89edafd5cdeb6b8
Laura Duarte <dduartelaureta@gmail.com>
2026-09-19 10:54:55 -0300

    adicionando base do carrinho

---
+++ encomendas.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'config/conexao.php';
+
+include 'includes/header.php';
+
+?>
+
+<h1>Meu Carrinho</h1>
+
+<p>
+    Confira os produtos que você adicionou ao carrinho.
+</p>
+
+<p>
+    Nenhum produto foi adicionado ao carrinho.
+</p>
+
+<?php
+
+include 'includes/footer.php';
+
+?>
