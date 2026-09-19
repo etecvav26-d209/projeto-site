f840ffb505d6d909d3b4cc25d85061ea39866ba8
Laura Duarte <dduartelaureta@gmail.com>
2026-09-19 13:17:49 -0300

    adicionando para caso não encontre produtos

--- encomendas.php
+++ encomendas.php
@@ -4,6 +4,9 @@ require_once 'config/conexao.php';
 include 'includes/header.php';
 $produtosSelecionados = $_POST['produtos'] ?? array();
 
+if(empty($produtosSelecionados)) {
+    echo "<p>Nenhum produto foi selecionado.</p>";
+}
 ?>
 
 <h1>Meu Carrinho</h1>
@@ -12,9 +15,6 @@ $produtosSelecionados = $_POST['produtos'] ?? array();
     Confira os produtos que você adicionou ao carrinho.
 </p>
 
-<p>
-    Nenhum produto foi adicionado ao carrinho.
-</p>
 
 <?php
 
