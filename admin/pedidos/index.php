9ee937c9b764a442d1c29c15a7c9ad4bf31bb812
Laura Duarte <dduartelaureta@gmail.com>
2026-09-18 21:01:01 -0300

    adicionando execução do sql

--- admin/pedidos/index.php
+++ admin/pedidos/index.php
@@ -8,4 +8,8 @@ $sql = "SELECT pedidos.*, usuarios.nome
         ON pedidos.usuario_id = usuarios.id
         ORDER BY pedidos.id DESC";
 
+$stmt = $conexao->prepare($sql);
+$stmt->execute();
+$pedidos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
