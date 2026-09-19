038fda93974e8a85636d535e09fbde1d63fd63f1
Laura Duarte <dduartelaureta@gmail.com>
2026-09-19 11:25:34 -0300

    adicionando execução

--- index.php
+++ index.php
@@ -4,4 +4,9 @@ require_once "config/conexao.php";
 include 'includes/header.php';
 
 $sql = "SELECT * FROM produtos WHERE disponivel = 1";
+
+$stmt = $conexao->prepare($sql);
+$stmt->execute();
+$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
