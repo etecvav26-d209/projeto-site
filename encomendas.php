5700fbdff24939f839e75f5258288b4a2bd72c7e
Laura Duarte <dduartelaureta@gmail.com>
2026-09-19 23:14:57 -0300

    adicionando formulario e botão

--- encomendas.php
+++ encomendas.php
@@ -6,18 +6,62 @@ $produtosSelecionados = $_POST['produtos'] ?? array();
 
 if(empty($produtosSelecionados)) {
     echo "<p>Nenhum produto foi selecionado.</p>";
-}
+} else {
 ?>
 
-<h1>Meu Carrinho</h1>
+    <form method="POST" action="encomendas.php">
 
-<p>
-    Confira os produtos que você adicionou ao carrinho.
-</p>
+        <?php
+        $total = 0;
 
+        foreach($produtosSelecionados as $id) {
+
+            $sql = "SELECT * FROM produtos WHERE id = :id";
+            $stmt = $conexao->prepare($sql);
+            $stmt->execute([
+                ':id' => $id
+            ]);
+
+            $produto = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($produto) {
+        ?>
+
+                <div class="produto">
+                    <h3>
+                        <?php echo $produto['nome']; ?>
+                    </h3>
+
+                    <p>
+                        <?php echo $produto['descricao']; ?>
+                    </p>
+
+                    <p>
+                        Preço:
+                        R$
+                        <?php echo number_format($produto['preco'], 2, ',', '.'); ?>
+                    </p>
+
+                    <label>
+                        Quantidade:
+                    </label>
+
+                    <input ype="number" name="quantidade[<?php echo $produto['id']; ?>]" value="1" min="1" >
+                </div>
+                <hr>
+
+        <?php
+            }
+        }
+        ?>
+
+
+        <button type="submit">
+            Atualizar carrinho
+        </button>
+    </form>
 
 <?php
-
+}
 include 'includes/footer.php';
-
 ?>
