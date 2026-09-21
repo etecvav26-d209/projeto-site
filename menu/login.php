d6356c13a99e8b2f649c07a74142a98f0fad9213
colocando parte em php <saraivabatistan@gmail.com>
2026-09-20 21:36:55 -0300

    colocando parte em php

--- menu/login.php
+++ menu/login.php
@@ -1,1 +1,33 @@
+<?php
 
+session_start();
+
+require_once '../config/conexao.php';
+include '../includes/header.php';
+
+if(isset($_POST['login'])) {
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM usuarios WHERE email = :email";
+
+    $stmt = $conexao->prepare($sql);
+
+    $stmt->execute([
+        ':email' => $email
+    ]);
+
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($usuario && password_verify($senha, $usuario['senha'])) {
+
+        $_SESSION['usuario_id'] = $usuario['id'];
+
+        echo "<p>Login realizado com sucesso!</p>";
+
+    } else {
+        echo "<p>Email ou senha incorretos.</p>";
+    }
+}
+?>
