ecf56ee7509d7db45df87dfefe2610a0381efbd3
colocando parte em php <saraivabatistan@gmail.com>
2026-09-20 22:30:36 -0300

    adicionando requisição para ser apenas tipo adm

--- admin/login.php
+++ admin/login.php
@@ -1,21 +1,37 @@
 <?php
+session_start();
+require_once '../config/conexao.php';
+include "../includes/header.php";
 
-require_once "../includes/header.php";
+if(isset($_POST['login'])) {
 
+    $email = $_POST['email'];
+    $senha = $_POST['senha']
+    $sql = "SELECT * FROM usuarios
+            WHERE email = :email
+            AND tipo = 'admin'";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute([
+        ':email' => $email
+    ]);
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($usuario && password_verify($senha, $usuario['senha'])) {
+
+        $_SESSION['usuario_id'] = $usuario['id'];
+
+        $_SESSION['tipo'] = $usuario['tipo'];
+
+        header('Location: index.php');
+        exit;
+
+    } else {
+
+     echo "<p>Email ou senha incorretos.</p>";
+    }
+}
 ?>
 
-<section class="admin-painel">
-
-    <h1>Login Administrativo</h1>
-
-    <p>
-        Área de acesso administrativo.
-    </p>
-
-</section>
-
 <?php
-
-require_once "../includes/footer.php";
-
+include "../includes/footer.php";
 ?>
