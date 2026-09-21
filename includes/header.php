13ff8ab5e97d61770c8328f4ddf23136d0cadc53
colocando parte em php <saraivabatistan@gmail.com>
2026-09-21 07:58:05 -0300

    adicionando uma nova referencia de css

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Noir & Sugar</title>
-
+    
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/projeto-site/css/style.css">
 </head>
 
