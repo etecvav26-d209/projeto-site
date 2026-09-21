378057d5d6d33b266905c1f074928c672ccc09f1
Laura Duarte <dduartelaureta@gmail.com>
2026-09-21 16:32:45 -0300

    colocando botao para voltar

--- admin/pedidos/excluir.php
+++ admin/pedidos/excluir.php
@@ -25,15 +25,17 @@ if (!isset($_POST['id'])) {
 $id = $_POST['id'];
 
 if (isset($_POST['confirmar'])) { 
-    $sql_itens = "DELETE FROM itens_pedido 
-    WHERE pedido_id = :pedido_id"; 
+    $sql_itens = "DELETE FROM itens
+    WHERE pedido_id = :pedido_id";
     $stmt_itens = $conexao->prepare($sql_itens); 
-    $stmt_itens->execute([ ':pedido_id' => $id ]); 
-    header("Location: index.php"); exit; 
+    $stmt_itens->execute([ ':pedido_id' => $id ]);  
     $sql = "DELETE FROM pedidos WHERE id = :id";
     $stmt = $conexao->prepare($sql); 
     $stmt->execute([ ':id' => $id ]); 
-        echo "Pedido excluído com sucesso."; 
+        echo "Pedido excluído com sucesso.";
+        echo "<form action='index.php'>";
+        echo "<button type='submit'>Voltar para pedidos</button>";
+        echo "</form>"; 
     exit; 
 }
 
