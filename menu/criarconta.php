bab0b8d27f54c394bd35394360959377af508109
colocando parte em php <saraivabatistan@gmail.com>
2026-09-20 22:05:54 -0300

    colocando proteção de senha e conexao com o banco

--- menu/criarconta.php
+++ menu/criarconta.php
@@ -1,1 +1,35 @@
+<?php
 
+require_once '../config/conexao.php';
+include '../includes/header.php';
+
+if(isset($_POST['cadastrar'])) {
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $senha = password_hash($senha, PASSWORD_DEFAULT);
+
+    try {
+
+        $sql = "INSERT INTO usuarios
+                (nome, email, senha, tipo)
+                VALUES
+                (:nome, :email, :senha, 'cliente')";
+        $stmt = $conexao->prepare($sql);
+        $stmt->execute([
+            ':nome' => $nome,
+            ':email' => $email,
+            ':senha' => $senha
+        ]);
+
+        echo "<p>Conta criada com sucesso!</p>";
+
+        echo "<p><a href='login.php'>Ir para o login</a></p>";
+
+    } catch(PDOException $erro) {
+        echo "<p>Email já cadastrado.</p>";
+    }
+}
+?>
